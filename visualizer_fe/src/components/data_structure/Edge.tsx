3f8f9d9d8dafb739432e959325948637d9c3363e
Andrea Galimberti <andrea.galimberti05@gmail.com>
2025-08-02 23:24:18 +0200

    implement Node and Edge components

--- visualizer_fe/src/components/data_structure/Edge.tsx
+++ visualizer_fe/src/components/data_structure/Edge.tsx
@@ -1,5 +1,62 @@
-function Edge(){
-
+interface Point{
+    x:number;
+    y:number;
 }
+
+
+interface EdgeProps{
+    from:Point;
+    to: Point;
+    directed: boolean;
+    label: string;
+}
+
+
+function Edge({from, to, directed = false, label}: EdgeProps){
+    //vector from point from to point to
+    const dx = to.x - from.x;
+    const dy = to.y - from.y;
+    //vector norm
+    const length = Math.sqrt(dx * dx + dy * dy);
+    //vector normalisation, it only represents the direction, regardless of distance.
+    const unitX = dx / length;
+    const unitY = dy / length;
+    
+    const offset = 24; //node radius
+    const startX = from.x + unitX * offset;
+    const startY = from.y + unitY * offset;
+    const endX = to.x - unitX * offset;
+    const endY = to.y - unitY * offset;
+
+    return (
+        <g>
+        <line
+            x1={startX}
+            y1={startY}
+            x2={endX}
+            y2={endY}
+            className="stroke-gray-400 stroke-2"
+        />
+        {directed && (
+            <polygon
+            points={`${endX},${endY} ${endX - 8 * unitX + 4 * unitY},${endY - 8 * unitY - 4 * unitX} ${endX - 8 * unitX - 4 * unitY},${endY - 8 * unitY + 4 * unitX}`}
+            className={"stroke-gray-400".replace('stroke', 'fill')}
+            />
+        )}
+        {label && (
+        <text
+          x={(startX + endX) / 2}
+          y={(startY + endY) / 2}
+          className="fill-gray-600 text-sm font-medium"
+          textAnchor="middle"
+          dy="-5"
+        >
+          {label}
+        </text>
+      )}
+        </g>
+    );
+};
+
 
 export default Edge;
--- visualizer_fe/src/components/data_structure/Node.tsx
+++ visualizer_fe/src/components/data_structure/Node.tsx
@@ -1,6 +1,27 @@
-function Node(){
+interface NodeProps{
+    value: string;
+    x: number;
+    y: number;
+    isFixed: boolean;
+    isCurrent: boolean;
+    isMin: boolean;
+}
+
+function Node({value, x, y, isFixed = false, isCurrent= false, isMin= false  }: NodeProps){
+
+    const bgColor = isCurrent
+            ? "bg-green-500"
+            : isMin
+            ? "bg-red-500"
+            : "bg-blue-500";
+  const border = isFixed ? "border-4 border-white": '';
     return (
-        
+        <div className={`absolute w-12 h-12 rounded-full 
+                      flex items-center justify-center text-white font-bold cursor-pointer
+                      ${bgColor} ${border}`}
+            style={{ left: x - 24, top: y - 24 }}>
+            <p>{value}</p>
+        </div>
     )
 }
 
